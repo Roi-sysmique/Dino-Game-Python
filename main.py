90bd2bd825d13802894fdeb55ab92c2e48493b9b
Roi-sysmique <elhechmihamza@gmail.com>
2024-05-14 20:46:59 +0100

    'Obstalce-added'

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pygame
+import random
 
 pygame.init()
 
-SCREEN_HEIGHT, SCREEN_WIDTH = 500, 800
-
+SCREEN_HEIGHT, SCREEN_WIDTH = 500, 1000
+font = pygame.font.Font(None, 50)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Dino game')
 clock = pygame.time.Clock()
 background = pygame.surface.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
 background.fill('black')
+start_time = 0
+lap = 0
+speed = -6
+interval = 15
 
 
 class Player(pygame.sprite.Sprite):
@@ -29,7 +34,41 @@ class Player(pygame.sprite.Sprite):
             self.rect.bottom = SCREEN_HEIGHT
 
 
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.surface.Surface((25, 100))
+        self.image.fill('yellow')
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+    def update(self):
+        global score, lap, speed, interval
+        augmentation = score - lap
+        print(f"{augmentation}: {lap}, {speed}: {interval}")
+        if augmentation >= 50 and speed != -10:
+            speed -= 1
+            interval -= 2
+            lap = score
+        self.rect.move_ip(speed, 0)
+        if self.rect.centerx <= -50:
+            self.kill()
+
+
 dino = Player()
+obstacle = Obstacle()
+obstacles = pygame.sprite.Group()
+obstacles.add(obstacle)
+
+
+def wave_systeme():
+    global start_time
+    break_time = int(pygame.time.get_ticks() / 100) - start_time
+    if break_time >= random.randint(interval, 15):
+        start_time = int(pygame.time.get_ticks() / 100)
+        spike_obstacle = Obstacle()
+        obstacles.add(spike_obstacle)
+
 
 while True:
     # event loop
@@ -38,10 +77,18 @@ while True:
             pygame.quit()
             quit()
 
+    score = int(pygame.time.get_ticks() / 100)
+    obstacles.update()
+    score_text = font.render(str(score), False, 'white')
+    score_rect = score_text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
     input_keys = pygame.key.get_pressed()
     screen.blit(background, (0, 0))
     screen.blit(dino.image, dino.rect)
+    obstacles.draw(screen)
+    screen.blit(score_text, score_rect)
     dino.update(input_keys)
+    wave_systeme()
+    obstacle.update()
 
     pygame.display.update()
     clock.tick(75)
